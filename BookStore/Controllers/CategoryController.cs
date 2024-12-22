06376f0e74964971905dacce9382bcae8ebe4588
RM798 <rameshmaurya798@gmail.com>
2024-12-22 22:51:17 +0530

    RM:Create functionality Added.

--- BookStore/Controllers/CategoryController.cs
+++ BookStore/Controllers/CategoryController.cs
@@ -12,7 +12,7 @@ namespace BookStore.Controllers
         {
             _db = db;
         }
-        public IActionResult Details()
+        public IActionResult Index()
         {
             List<Category> objcategories = _db.CategoryRecord.ToList();
             if (objcategories == null)
@@ -20,6 +20,49 @@ namespace BookStore.Controllers
                 return NotFound();
             }
             return View(objcategories);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            //if(obj.Name == obj.DisplayOrder.ToString())
+            //{
+            //    ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name.");
+            //}
+            //if (obj.Name!=null && obj.Name.ToLower() == "test")
+            //{
+            //    ModelState.AddModelError("", "Test is an invalid value");
+            //}
+            if (ModelState.IsValid)
+            {
+                _db.CategoryRecord.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }         
+            return View();
+
+        }
+
+        [HttpPost]
+        //[ValidateAntiForgeryToken]
+        public Category Save(Category details)
+        {
+            Category newdata = new Category();
+            newdata.Name = details.Name;
+            newdata.DisplayOrder = details.DisplayOrder;
+            if (ModelState.IsValid)
+            {
+                _db.CategoryRecord.Add(newdata); // Add to context
+                _db.SaveChanges();          // Save changes to database
+                //return RedirectToAction("Index"); // Redirect to a confirmation page or list
+            }
+
+            return newdata; // Return to the form if validation fails
         }
     }
 }
