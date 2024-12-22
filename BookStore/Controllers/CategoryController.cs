f8821bb5f09ce3f33e71776c4b79c8419844818b
RM798 <rameshmaurya798@gmail.com>
2024-12-22 13:15:10 +0530

    Add project files.

---
+++ BookStore/Controllers/CategoryController.cs
@@ -0,0 +1,25 @@
+﻿using BookStore.Data;
+using BookStore.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStore.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Details()
+        {
+            List<Category> objcategories = _db.CategoryRecord.ToList();
+            if (objcategories == null)
+            {
+                return NotFound();
+            }
+            return View(objcategories);
+        }
+    }
+}
---
+++ BookStore/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using BookStore.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStore.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Category> CategoryRecord { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                    new Category { Id=1, Name="Action", DisplayOrder = 1},
+                    new Category { Id=2, Name="Scifi", DisplayOrder=2},
+                    new Category { Id=3, Name="History", DisplayOrder=4}
+                );
+        }
+    }
+}
